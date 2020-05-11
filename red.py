3ad69bd3d17cd069c908472ca23163c043a9aa80
Casey MacPherson <casey@macphersonclan.org>
2020-05-11 16:52:11 +0100

    The Green color

---
+++ red.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import time
+import unicornhat as unicorn
+
+
+unicorn.set_layout(unicorn.AUTO)
+unicorn.rotation(0)
+unicorn.brightness(0.5)
+width,height=unicorn.get_shape()
+
+
+for y in range(height):
+    for x in range(width):
+        unicorn.set_pixel(x,y,255,0,0)
+
+unicorn.show()
+
+while True:
+    time.sleep(1)
