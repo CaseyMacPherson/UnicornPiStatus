5bdcd383959688485815fa4a254595353e011cf3
Casey MacPherson <casey@macphersonclan.org>
2020-05-14 20:45:35 +0100

    Making it less intense, set brightness to 1 and the red value to 48

--- red.py
+++ red.py
@@ -6,13 +6,13 @@ import unicornhat as unicorn
 
 unicorn.set_layout(unicorn.AUTO)
 unicorn.rotation(0)
-unicorn.brightness(0.5)
+unicorn.brightness(1.0)
 width,height=unicorn.get_shape()
 
 
 for y in range(height):
     for x in range(width):
-        unicorn.set_pixel(x,y,255,0,0)
+        unicorn.set_pixel(x,y,48,0,0)
 
 unicorn.show()
 
